877ae00665bc59538f0cddf2bba26cb652a343f3
juan <megactivoai@gmail.com>
2025-04-24 11:50:51 -0500

    add gemini completion rote

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from google import genai
 
 app = Flask(__name__)
 CORS(app)
+
+client = genai.Client(api_key="AIzaSyARW29vXwnRaaIIbIeiHi5S8Nx7hhiMsAo")
 
 # get all friends
 @app.route("/",methods=["GET"])
@@ -20,6 +23,22 @@ def say_hello():
         return jsonify({"hello": "hello "+name}), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+# create a completion
+@app.route("/completion",methods=["POST"])
+def create_completion():
+    try:
+        data = request.json
+        question = data.get("question")
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash", contents=question
+        )
+
+        return jsonify({"answer": response.text}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run()
